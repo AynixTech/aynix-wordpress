2930d0206052b5f1d0a316c564dc75a9df6a975b
Anthony Avila <50991131+jordanavila1394@users.noreply.github.com>
2026-02-04 14:38:56 +0100

    Tradotte label telefoni (Italia/Europa e America Latina) in tutte le lingue per pagina Contattaci

--- wp-content/themes/aynix/page-contattaci.php
+++ wp-content/themes/aynix/page-contattaci.php
@@ -147,8 +147,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['contact_form_submit']
                 <strong>AYNIX SRL</strong><br>
                 Via Populonia, 8 – 20159 Milano (MI)<br>
                 Email: <a href="mailto:info@aynix.tech">info@aynix.tech</a><br><br>
-                <strong>Italia / Europa:</strong> <a href="tel:<?php echo str_replace(' ', '', aynix_translate('contact.phone_europe')); ?>"><?php echo aynix_translate('contact.phone_europe'); ?></a><br>
-                <strong>America Latina:</strong> <a href="tel:<?php echo str_replace(' ', '', aynix_translate('contact.phone_latin_america')); ?>"><?php echo aynix_translate('contact.phone_latin_america'); ?></a>
+                <strong><?php echo aynix_translate('contact.phone_label_europe'); ?>:</strong> <a href="tel:<?php echo str_replace(' ', '', aynix_translate('contact.phone_europe')); ?>"><?php echo aynix_translate('contact.phone_europe'); ?></a><br>
+                <strong><?php echo aynix_translate('contact.phone_label_latam'); ?>:</strong> <a href="tel:<?php echo str_replace(' ', '', aynix_translate('contact.phone_latin_america')); ?>"><?php echo aynix_translate('contact.phone_latin_america'); ?></a>
             </p>
         </section>
     </div>
